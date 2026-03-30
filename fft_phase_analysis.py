ff507e74b8c252027f5329b12e10e9da4db74e6e
Matthew <buchkowski@protonmail.com>
2026-03-29 21:35:29 -0400

    removed unused files. updated .gitignore. Updated nad cleaned up the fft phase analysis files.

--- fft_phase_analysis.py
+++ fft_phase_analysis.py
@@ -10,26 +10,38 @@ from scipy.optimize import curve_fit
 from itertools import combinations
 
 
-WINDOWS_SIZE = 60
-STEP_SIZE = 10
-HZ = 10
+WINDOWS_SIZE = 60           #Window size for sliding window correlation (in frames)
+STEP_SIZE = 10              #Step size for sliding window correlation (in frames)
+HZ = 10                     #Frequency of LED
 EXPOSURE_TIME = 0.040       #units of seconds
+
+
 #the effective readout rate is negligible because of frame transfer. 
 # So our exposure time is effectively the frame time, which is 1/Hz. 
 # So we can use this to estimate how many frames correspond to one period of the signal, which can help us 
 #       choose appropriate window sizes for the sliding window correlation.
 
-PERIOD = 1 / HZ 
+PERIOD = 1 / HZ             #Period of the LED signal in seconds
 
 boxes_filepath = "./boxes.json"
 
 
 
 #================================================================================
+"""
+Helper functions for FFT-based phase analysis of camera signals.
+"""
 
+"""
+Linear model for curve fitting Phase Drift (Clock Drift) over time.
+"""
 def linear_model(x, a, b):
     return a * x + b
 
+"""
+Lists files in a directory, excluding subdirectories. 
+If the directory does not exist, it raises an error.
+"""
 def list_files_classic(directory_path):
     '''
     List all files in a directory, excluding subdirectories.'''
@@ -41,10 +53,16 @@ def list_files_classic(directory_path):
     
     return files
 
+"""
+Load in FITS data from a given file path and return the data array.
+"""
 def load_data(file_path):
     hdul = fits.open(file_path)
     return hdul[0].data
 
+"""
+Calculate mean intensities within a specified box region for each frame in the FITS file.
+"""
 def cal_intensities(file, cam_id, box):
     data = load_data(file)
     xmin, xmax, ymin, ymax = box['xmin'], box['xmax'], box['ymin'], box['ymax']
@@ -52,21 +70,29 @@ def cal_intensities(file, cam_id, box):
 
     return intensities
 
+"""
+Confirm that a folder path exists; if it doesn't, create it
+"""
 def confirm_folder_path(folder_path):
     """Ensure that the folder exists; create it if it doesn't."""
     if os.path.exists(folder_path):
         if os.path.isdir(folder_path):
             pass
-        else:
-            raise FileExistsError(f"A file exists at '{folder_path}', cannot create folder.")
     else:
         os.makedirs(folder_path, exist_ok=True)
         print(f"The folder '{folder_path}' has been created.")
  
+"""
+Perform a sub frame estimation of cross correltaion peak.
+"""
 def subframe_peak_location(correlation):
     """
     Refines peak location using parabolic interpolation
     to achieve sub-frame accuracy.
+
+    Parabolic interpolation is used because the peaks from the cross correltaion are shaped
+    like a parabola. By fitting a parabola to the peak and its immediate neighbors,
+    we can estimate the true peak location.
     """
     peak = np.argmax(correlation)
 
@@ -119,17 +145,17 @@ def sliding_window_correlation(main_sig, other_sig, window_size=60, step=10, mai
         lags.append(raw_lag)
         time_indices.append(start)
         
-    if plot and main_camera_id is not None and other_camera_id is not None and save_path is not None and file_name is not None:
+    # if plot and main_camera_id is not None and other_camera_id is not None and save_path is not None and file_name is not None:
 
-        fig3 = plt.figure(figsize=(10, 4))
-        plt.plot(time_indices, lags, 'o-')
-        plt.title("Intra-file Clock Drift Analysis")
-        plt.xlabel("Frame Number (Start of Window)")
-        plt.ylabel("Measured Lag (Frames)")
-        plt.grid(True)
-        plt.savefig(f"{save_path}/{main_camera_id}vs{other_camera_id}/Intra-file_Clock_Drift__{file_name}.png")
-        # plt.show()
-        plt.close(fig3)
+    #     fig3 = plt.figure(figsize=(10, 4))
+    #     plt.plot(time_indices, lags, 'o-')
+    #     plt.title("Intra-file Clock Drift Analysis")
+    #     plt.xlabel("Frame Number (Start of Window)")
+    #     plt.ylabel("Measured Lag (Frames)")
+    #     plt.grid(True)
+    #     plt.savefig(f"{save_path}/{main_camera_id}vs{other_camera_id}/Intra-file_Clock_Drift__{file_name}.png")
+    #     # plt.show()
+    #     plt.close(fig3)
 
 
     return time_indices, lags
@@ -185,7 +211,7 @@ def fft_cross_correlation(main_intensities, other_intensities, main_camera_id=No
         fig2 = plt.figure(figsize=(10, 5))
         plt.title("Cross-Correlation between Camera {} and Camera {}".format(main_camera_id, other_camera_id))
         plt.plot(correlation)
-        plt.vlines(np.argmax(correlation), 0, max_peak, color='r', label='Peak')
+        plt.vlines(np.argmax(correlation), 0, correlation[np.argmax(correlation)], color='r', label='Peak')
         plt.xlabel("Lag (frames)")
         plt.ylabel("Cross-Correlation")
         plt.legend()
@@ -277,12 +303,48 @@ def plot_drift_fit(indices, lags, popt,
         f"{save_path}/{main_camera_id}vs{other_camera_id}"
     )
 
+    text_str = f"Slope: {popt[0]:.4f} frames/frame\nIntercept: {popt[1]:.2f} frames"
+    plt.text(0.05, 0.95, text_str, transform=plt.gca().transAxes, fontsize=10,
+             verticalalignment='top', bbox=dict(boxstyle='round', facecolor='white', alpha=0.5))
+
     plt.savefig(
         f"{save_path}/{main_camera_id}vs{other_camera_id}/"
         f"drift_fit__{file_name}.png"
     )
 
     plt.close(fig7)
+
+
+def gather_intensities(main_camera_path, other_camera_path, main_box, other_box, main_camera, other_cam_id, file):
+    main_intensities = cal_intensities(
+        os.path.join(main_camera_path, file),
+        main_camera,
+        main_box
+    )
+
+    other_intensities = cal_intensities(
+        os.path.join(other_camera_path, other_file),
+        other_cam_id,
+        other_box
+    )
+
+    return main_intensities, other_intensities
+
+
+def curve_fit_phase_drift(indices, lags, plot_data):
+    popt, pcov = curve_fit(linear_model, indices, lags)
+    a_fit, b_fit = popt
+    a_err, b_err = np.sqrt(np.diag(pcov))
+    
+    if plot_data:
+        plot_drift_fit(
+            indices, lags, popt,
+            main_camera_id=main_camera,
+            other_camera_id=other_cam_id,
+            save_path=f"./fft_phase_analysis/{Hz}Hz",
+            file_name=f"{file.split('.')[0]}"
+        )
+    return popt, pcov
 
 
 if __name__ == "__main__":
@@ -304,7 +366,6 @@ if __name__ == "__main__":
 
     for main_camera, other_cam_id in camera_pairs:
 
-        changed = True
 
         main_camera_path = f"./Data/{main_camera}/3D_CUBE/{Hz}Hz"
         other_camera_path = f"./Data/{other_cam_id}/3D_CUBE/{Hz}Hz"
@@ -315,14 +376,9 @@ if __name__ == "__main__":
         main_box = boxes[f"{Hz}Hz"][str(main_camera)]
         other_box = boxes[f"{Hz}Hz"][str(other_cam_id)]
 
+        plot_data = True
+
         for file in main_camera_files:
-
-            main_intensities = cal_intensities(
-                os.path.join(main_camera_path, file),
-                main_camera,
-                main_box
-            )
-
             for other_file in other_camera_files:
 
 
@@ -334,10 +390,12 @@ if __name__ == "__main__":
                 print(f"Files left to process: {total_files_to_process - processed_sofar - 1}")
 
 
-                other_intensities = cal_intensities(
-                    os.path.join(other_camera_path, other_file),
-                    other_cam_id,
-                    other_box
+
+                main_intensities, other_intensities = gather_intensities(
+                    main_camera_path, other_camera_path,
+                    main_box, other_box,
+                    main_camera, other_cam_id,
+                    file
                 )
 
                 max_peak, phase_offset = fft_cross_correlation(
@@ -345,7 +403,7 @@ if __name__ == "__main__":
                     other_intensities,
                     main_camera_id=main_camera,
                     other_camera_id=other_cam_id,
-                    plot=True,
+                    plot=plot_data,
                     save_path=f"./fft_phase_analysis/{Hz}Hz",
                     file_name=f"{file.split('.')[0]}"
                 )
@@ -357,22 +415,14 @@ if __name__ == "__main__":
                     step=STEP_SIZE,
                     main_camera_id=main_camera,
                     other_camera_id=other_cam_id,
-                    plot=True,
+                    plot=plot_data,
                     save_path=f"./fft_phase_analysis/{Hz}Hz",
                     file_name=f"{file.split('.')[0]}"
                 )
 
-                popt, pcov = curve_fit(linear_model, indices, lags)
+                popt, pcov = curve_fit_phase_drift(indices, lags, plot_data)
                 a_fit, b_fit = popt
                 a_err, b_err = np.sqrt(np.diag(pcov))
-
-                plot_drift_fit(
-                    indices, lags, popt,
-                    main_camera_id=main_camera,
-                    other_camera_id=other_cam_id,
-                    save_path=f"./fft_phase_analysis/{Hz}Hz",
-                    file_name=f"{file.split('.')[0]}"
-                )
 
                 time_offset = phase_offset * EXPOSURE_TIME
                 phase_deg = (360 * time_offset / PERIOD)
@@ -390,8 +440,10 @@ if __name__ == "__main__":
                     "Intercept": float(b_fit),
                     "Slope Error": float(a_err),
                     "Intercept Error": float(b_err),
-                    "File Pair": f"{file} vs {other_file}"
+                    "File": f"{file}"
                 })
+
+                plot_data = False
 
 
 
@@ -400,7 +452,7 @@ if __name__ == "__main__":
         new_df = pd.DataFrame(results_list)
 
         phase_df = pd.concat([phase_df, new_df], ignore_index=True)
-
+        results_list = []
         
 
     phase_df.to_csv(f"./fft_phase_analysis/{Hz}Hz/phase_analysis_results.csv", index=False)
